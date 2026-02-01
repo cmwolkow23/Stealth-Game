798e040f0e2bd9125273e810e921defd27771c56
LandonK807 <Landonkokales@gmail.com>
2026-01-31 20:18:15 -0600

    New Scene Pick Up Shit
    Please just fucking kill me blow my brains out onto the ceiling i cant take this anymore colby has bascially programmed the entire game at this point please just make it stop

--- Assets/Scripts/PlayerPickUp.cs
+++ Assets/Scripts/PlayerPickUp.cs
@@ -1,70 +1,52 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerPickup : MonoBehaviour
+public class ItemPickup : MonoBehaviour
 {
     [Header("Pickup Settings")]
-    public float pickupRange = 3f;
-    public LayerMask pickupLayers;
-    public Transform cameraTransform;
+    public float pickupRange = 2f;
+    public LayerMask pickupLayer;
+    public Transform rayOrigin; // usually camera
+    private InputSystem_Actions interactAction;
 
-    private InputSystem_Actions inputActions;
-
-    public interface IPickup
+    private void Start()
     {
-        void OnPickup(GameObject player);
+        interactAction = new InputSystem_Actions();
+        interactAction.Enable();
     }
 
-    void Awake()
+    private void Update()
     {
-        inputActions = new InputSystem_Actions();
-    }
-
-    void OnEnable()
-    {
-        inputActions.Enable();
-        inputActions.Player.Interact.performed += OnPickupPerformed;
-    }
-
-    void OnDisable()
-    {
-        inputActions.Player.Interact.performed -= OnPickupPerformed;
-        inputActions.Disable();
-    }
-
-    private void OnPickupPerformed(InputAction.CallbackContext context)
-    {
-        TryPickup();
-    }
-
-    void TryPickup()
-    {
-        if (cameraTransform == null)
+        if(interactAction.Player.Interact.WasPressedThisFrame())
         {
-            Debug.LogWarning("cameraTransform is not assigned!");
-            return;
+            OnInteract();
         }
+    }
+    public void OnInteract()
+    {
+        //if (!context.performed)
+           // return;
+        Debug.Log("Interact action performed");
+        Ray ray = new Ray(rayOrigin.position, rayOrigin.forward);
 
-        Ray ray = new Ray(cameraTransform.position, cameraTransform.forward);
-        Debug.DrawRay(cameraTransform.position, cameraTransform.forward * pickupRange, Color.green, 1f);
+        if (Physics.Raycast(ray, out RaycastHit hit, pickupRange, pickupLayer))
+        {
+            Destroy(hit.collider.gameObject);
+        }
+        
+    }
 
-        if (Physics.Raycast(ray, out RaycastHit hit, pickupRange, pickupLayers))
-        {
-            Debug.Log($"Raycast hit: {hit.collider.name}");
-            IPickup pickup = hit.collider.GetComponent<IPickup>();
-            if (pickup != null)
-            {
-                pickup.OnPickup(gameObject);
-            }
-            else
-            {
-                Debug.Log("Hit object does not implement IPickup.");
-            }
-        }
-        else
-        {
-            Debug.Log("Raycast did not hit anything.");
-        }
+    void OnDrawGizmos()
+    {
+        if (rayOrigin == null) return;
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(
+            rayOrigin.position,
+            rayOrigin.position + rayOrigin.forward * pickupRange
+        );
     }
 }
+
+
 
