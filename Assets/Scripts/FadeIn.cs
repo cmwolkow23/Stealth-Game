00ae7a5dad1932fdc9a4167862d7131faa548615
Phin Patterson <phin.patterson@trojans.dsu.edu>
2026-01-31 23:00:18 -0600

    Cleaned up title screen UI, added quit button, added fade in effect to title screen

---
+++ Assets/Scripts/FadeIn.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    public float duration = 2f;
+    private Image img;
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        StartCoroutine(Fade());
+    }
+
+    System.Collections.IEnumerator Fade()
+    {
+        float t = 0;
+        Color c = img.color;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            c.a = 1 - (t / duration);
+            img.color = c;
+            yield return null;
+        }
+
+        c.a = 0;
+        img.color = c;
+    }
+}
